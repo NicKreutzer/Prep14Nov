efcfa2cd5e1e6e419fc183bc64fdf0fb9649d2ab
NicKreutzer <nicolaskreutzer@hotmail.com.ar>
2022-11-24 13:48:42 -0300

    Test 3

--- Prep-Course-B-main/03-JS-III/homework/homework.js
+++ Prep-Course-B-main/03-JS-III/homework/homework.js
@@ -8,29 +8,66 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+
+    // var suma = 0
+    // var i = 1
+    // while (i < 11){
+    // suma = suma + i;
+    // i++
+    //}
+    // return suma
+
+// var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// var suma = 0
+// for (var element of numeros){
+// suma = element + suma
+// }
+// return suma;
+
+// var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// var suma = 0
+// numeros.forEach((element) => {
+// suma = suma + element;
+// });
+// return suma;
+
+
+    var suma = 0
+  for (var i = 1; i < 11; i++){
+    suma = suma + i
+  }
+  return suma
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((element)=> element % 2 ===0);
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+return array.map((element) => element ** 2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((acumulador,siguiente) => acumulador + siguiente)
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  
+ // var numeroEnString = num.toString();
+ // var longitud = numeroEnString.length;
+ // return longitud;
+ return num.toString().length
 }
   
   
